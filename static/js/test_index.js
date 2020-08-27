bfcf5687137dbefc40ab59223d05b3d9fd41b64e
Rupali Roy <48709625+rroy1212@users.noreply.github.com>
2020-08-26 23:38:15 -0500

    Add files via upload

---
+++ static/js/test_index.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+    // Init
+    // $('.loader').hide();
+    $('#result').hide();
+
+
+    // Predict
+    $('#btn-predict').click(function () {
+
+        // Show loading animation
+        // $('.loader').show();
+        $('#result').show();
+
+
+    });
+
+});
